f08305144812f84a73336b0edc54ae4c1c258036
Safwan Islam Rupam <safwanislamrupam10@gmail.com>
2025-09-30 00:25:55 +0600

    Update Operator.c

--- Operator.c
+++ Operator.c
@@ -208,3 +208,24 @@ printf("D 2.00");
 else
 printf("F 0”);
 }
+//Bitwise Operator  
+#include<stdio.h>
+    int main()
+{
+    
+int a = 32;
+int b = 12;  
+int c;
+
+    c = a&b;
+    printf("a&b = %d\n",c);
+
+    c = a|b;
+    printf("a|b = %d\n",c);
+
+    c = a^b;
+    printf("a^b = %d\n",c);
+    
+
+
+}
