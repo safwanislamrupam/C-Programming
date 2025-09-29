4019e994172bb0b852f2cb1e37ef69d994da332e
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-09-29 23:27:18 +0600

    Update Operator.c

--- Operator.c
+++ Operator.c
@@ -17,3 +17,22 @@ int main()
     printf("%d * %d = %d\n",a,b,mul);
 
 }
+//Assignment Opearator  
+#include<stdio.h>
+int main()
+{
+     float a = 5;  
+     a+=2;  // a =  a  + 2 (for addition)
+    printf("%f\n",a);  
+
+    a-=2; // a =  a  - 2 (for subtraction)
+    printf("%f\n",a);
+
+    a*=2; // a =  a  * 2 (for multipication)
+    printf("%f\n",a);
+
+    a/=2; // a =  a  / 2 (for division)
+    printf("%f\n",a);
+
+
+}
